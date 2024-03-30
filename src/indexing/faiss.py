f878c067c0a26c02d675e7c111f041c429b3258d
Joel Rorseth <joelrorseth@gmail.com>
2024-03-30 19:34:30 +0000

    Add function to load faiss index

--- src/indexing/faiss.py
+++ src/indexing/faiss.py
@@ -1,8 +1,15 @@
 import logging
 import faiss
 import shutil
+from pathlib import Path
 from typing import List
-from llama_index.core import Document, VectorStoreIndex, Settings, StorageContext
+from llama_index.core import (
+    Document,
+    VectorStoreIndex,
+    Settings,
+    StorageContext,
+    load_index_from_storage
+)
 from llama_index.vector_stores.faiss import FaissVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from config import HF_EMBEDDING_MODEL_DIM, HF_EMBEDDING_MODEL_NAME, INDEX_DATA_PATH
@@ -10,12 +17,16 @@ from src.indexing.strategy import IndexingStrategy, get_index_textbook_groupings
 from src.preprocessing.textbook import PreprocessedTextbook
 
 
+def get_index_path(index_name: str, indexing_strategy: IndexingStrategy) -> Path:
+    return INDEX_DATA_PATH / indexing_strategy.value / index_name
+
+
 def build_index(index_name: str, strategy: IndexingStrategy,
                 textbooks: List[PreprocessedTextbook], overwrite: bool = False):
     """
     Build a FAISS VectorStoreIndex for a set of textbooks.
     """
-    index_path = INDEX_DATA_PATH / strategy.value / index_name
+    index_path = get_index_path(index_name, strategy)
 
     # Handle existing index
     if index_path.exists() and index_path.is_dir():
@@ -89,3 +100,15 @@ def build_all_indexes_for_all_strategies(
     logging.info("Building all indexes for all strategies")
     for strategy in IndexingStrategy:
         build_all_indexes_for_strategy(strategy, all_textbooks, overwrite)
+
+
+def load_index(index_name: str, strategy: IndexingStrategy) -> VectorStoreIndex:
+    """
+    Load a FAISS VectorStoreIndex.
+    """
+    index_path = get_index_path(index_name, strategy)
+    vector_store = FaissVectorStore.from_persist_dir(index_path)
+    storage_context = StorageContext.from_defaults(
+        vector_store=vector_store, persist_dir=index_path
+    )
+    return load_index_from_storage(storage_context=storage_context)
